d110ae8a3a28c92398e07ffca29cdce756354a0b
TakaIshikawa <hiro52ishikawa.us@gmail.com>
2026-04-28 20:16:56 +0900

    Create exporter registry

--- src/blueprint/exporters/__init__.py
+++ src/blueprint/exporters/__init__.py
@@ -8,9 +8,12 @@ from blueprint.exporters.azure_devops_csv import AzureDevOpsCsvExporter
 from blueprint.exporters.brief_review import BriefReviewPacketExporter
 from blueprint.exporters.calendar import CalendarExporter
 from blueprint.exporters.checklist import ChecklistExporter
+from blueprint.exporters.claude_code import ClaudeCodeExporter
 from blueprint.exporters.clickup_csv import ClickUpCsvExporter
+from blueprint.exporters.codex import CodexExporter
 from blueprint.exporters.coverage_matrix import CoverageMatrixExporter
 from blueprint.exporters.critical_path_report import CriticalPathReportExporter
+from blueprint.exporters.csv_tasks import CsvTasksExporter
 from blueprint.exporters.dependency_matrix import DependencyMatrixExporter
 from blueprint.exporters.file_impact_map import FileImpactMapExporter
 from blueprint.exporters.gantt import GanttExporter
@@ -20,21 +23,35 @@ from blueprint.exporters.github_projects_csv import GitHubProjectsCsvExporter
 from blueprint.exporters.gitlab_issues import GitLabIssuesExporter
 from blueprint.exporters.html_report import HtmlReportExporter
 from blueprint.exporters.jira_csv import JiraCsvExporter
+from blueprint.exporters.junit_tasks import JUnitTasksExporter
 from blueprint.exporters.kanban import KanbanExporter
 from blueprint.exporters.linear import LinearExporter
 from blueprint.exporters.manifest import ExportManifestExporter
+from blueprint.exporters.mermaid import MermaidExporter
 from blueprint.exporters.milestone_summary import MilestoneSummaryExporter
 from blueprint.exporters.notion_markdown import NotionMarkdownExporter
 from blueprint.exporters.plan_snapshot import PlanSnapshotExporter
 from blueprint.exporters.raci_matrix import RaciMatrixExporter
+from blueprint.exporters.registry import (
+    ExporterRegistration,
+    create_exporter,
+    get_exporter_registration,
+    resolve_target_name,
+    supported_target_aliases,
+    supported_target_names,
+)
+from blueprint.exporters.relay import RelayExporter
 from blueprint.exporters.release_notes import ReleaseNotesExporter
 from blueprint.exporters.relay_yaml import RelayYamlExporter
 from blueprint.exporters.risk_register import RiskRegisterExporter
 from blueprint.exporters.sarif_audit import SarifAuditExporter
 from blueprint.exporters.slack_digest import SlackDigestExporter
+from blueprint.exporters.smoothie import SmoothieExporter
 from blueprint.exporters.source_brief import SourceBriefExporter
 from blueprint.exporters.source_manifest import SourceManifestExporter
+from blueprint.exporters.status_report import StatusReportExporter
 from blueprint.exporters.status_timeline import StatusTimelineExporter
+from blueprint.exporters.task_bundle import TaskBundleExporter
 from blueprint.exporters.taskfile import TaskfileExporter
 from blueprint.exporters.task_queue_jsonl import TaskQueueJsonlExporter
 from blueprint.exporters.task_roster import TaskRosterExporter
@@ -51,11 +68,15 @@ __all__ = [
     "BriefReviewPacketExporter",
     "CalendarExporter",
     "ChecklistExporter",
+    "ClaudeCodeExporter",
     "ClickUpCsvExporter",
+    "CodexExporter",
     "CoverageMatrixExporter",
     "CriticalPathReportExporter",
+    "CsvTasksExporter",
     "DependencyMatrixExporter",
     "ExportManifestExporter",
+    "ExporterRegistration",
     "FileImpactMapExporter",
     "GanttExporter",
     "GitHubActionsExporter",
@@ -64,24 +85,35 @@ __all__ = [
     "GitLabIssuesExporter",
     "HtmlReportExporter",
     "JiraCsvExporter",
+    "JUnitTasksExporter",
     "KanbanExporter",
     "LinearExporter",
+    "MermaidExporter",
     "MilestoneSummaryExporter",
     "NotionMarkdownExporter",
     "PlanSnapshotExporter",
     "RaciMatrixExporter",
+    "RelayExporter",
     "ReleaseNotesExporter",
     "RelayYamlExporter",
     "RiskRegisterExporter",
     "SarifAuditExporter",
     "SlackDigestExporter",
+    "SmoothieExporter",
     "SourceBriefExporter",
     "SourceManifestExporter",
+    "StatusReportExporter",
     "StatusTimelineExporter",
+    "TaskBundleExporter",
     "TaskfileExporter",
     "TaskQueueJsonlExporter",
     "TaskRosterExporter",
     "TrelloJsonExporter",
     "VSCodeTasksExporter",
     "WaveScheduleExporter",
+    "create_exporter",
+    "get_exporter_registration",
+    "resolve_target_name",
+    "supported_target_aliases",
+    "supported_target_names",
 ]
